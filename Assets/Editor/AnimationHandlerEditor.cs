24d570512d7c9bdd710006df84a9a709e56f3ce4
rezky-ashari <32387444+rezky-ashari@users.noreply.github.com>
2018-05-16 11:31:18 +0700

    Update library.

---
+++ Assets/Editor/AnimationHandlerEditor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(AnimationHandler))]
+public class AnimationHandlerEditor : Editor {
+
+    static string stateName;
+
+    GUIContent onClickPlayLabel;
+    GUIContent clickIDLabel;
+    GUIContent onStartLabel;
+    GUIContent onCompleteLabel;
+    SerializedProperty onStartProperty;
+    SerializedProperty onCompleteProperty;
+
+    private void OnEnable()
+    {
+        onClickPlayLabel = new GUIContent("Play On Click", "Animation state to play when clicked.");
+        clickIDLabel = new GUIContent("Click ID", "ID to send to the scene's click event.");
+        onStartLabel = new GUIContent("On Start", "Methods to execute when 'OnStartAnimation' is called from AnimationEvent");
+        onCompleteLabel = new GUIContent("On Complete", "Methods to execute when 'OnCompleteAnimation' is called from AnimationEvent");
+        onStartProperty = serializedObject.FindProperty("onStart");
+        onCompleteProperty = serializedObject.FindProperty("onComplete");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        AnimationHandler animationHandler = (AnimationHandler)target;
+
+        animationHandler.onClickPlay = EditorGUILayout.TextField(onClickPlayLabel, animationHandler.onClickPlay);
+        animationHandler.clickID = EditorGUILayout.TextField(clickIDLabel, animationHandler.clickID);
+
+        if (Application.isPlaying)
+        {
+            EditorGUILayout.BeginHorizontal();
+            //EditorGUILayout.LabelField("State Name", GUILayout.Width(80));
+            stateName = EditorGUILayout.TextField("Play Animation", stateName);
+            if (GUILayout.Button("Play", GUILayout.Width(60)))
+            {
+                animationHandler.Play(stateName);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+        else
+        {
+            EditorGUILayout.HelpBox("You can test playing an animation in Play mode.", MessageType.Info);
+        }
+
+        EditorGUILayout.PropertyField(onStartProperty, onStartLabel);
+        EditorGUILayout.PropertyField(onCompleteProperty, onCompleteLabel);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/Utilities/AnimationHandler.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationHandler : MonoBehaviour, IPointerClickHandler
+{
+    /// <summary>
+    /// Animation state to play when clicked.
+    /// </summary>
+    public string onClickPlay;
+    /// <summary>
+    /// ID to send to the scene's click event.
+    /// </summary>
+    public string clickID;
+    public UnityEvent onStart;
+    public UnityEvent onComplete;
+
+    Animator _animator;
+    Animator Animator
+    {
+        get
+        {
+            if (_animator == null) _animator = GetComponent<Animator>();
+            return _animator;
+        }
+    }
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PlaySound(string name)
+    {
+        if (name.Contains("|"))
+        {
+            string[] names = name.Split('|');
+            Resound.PlaySFX(names[Random.Range(0, names.Length)].Trim());
+        }
+        else if (name.Contains(","))
+        {
+            string[] names = name.Split(',');
+            for (int i = 0; i < names.Length; i++)
+            {
+                Resound.PlaySFX(names[i].Trim());
+            }
+        }
+        else
+        {
+            Resound.PlaySFX(name);
+        }
+        
+    }
+
+    void PlaySoundLoop(string name)
+    {
+        Resound.PlaySFX(name, true);
+    }
+
+    void OnStartAnimation()
+    {
+        onStart.Invoke();
+    }
+
+    void OnCompleteAnimation()
+    {
+        onComplete.Invoke();
+    }
+
+    /// <summary>
+    /// Play an animation state.
+    /// </summary>
+    /// <param name="stateName"></param>
+    public void Play(string stateName)
+    {
+        Animator.Play(stateName);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!string.IsNullOrEmpty(onClickPlay)) Animator.Play(onClickPlay);
+        if (!string.IsNullOrEmpty(clickID)) Scene.SendClickEvent(clickID);
+    }
+}
