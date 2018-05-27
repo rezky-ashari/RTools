81aa0d346564ecc30c0c24328fdfdd92e3c39cf2
Rezky <rezuky@live.com>
2018-05-28 01:25:18 +0700

    Update library.

--- Assets/Editor/ToDoList.cs
+++ Assets/Editor/ToDoList.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+public class ToDoList : ScriptableObject
+{
+    public List<Item> list = new List<Item>();
+
+    [System.Serializable]
+    public class Item
+    {
+        public bool isDone;
+        public string task;
+    }
+}
+
 [CustomEditor(typeof(ToDoList))]
-public class ToDoListEditor : Editor {
+public class ToDoListEditor : Editor
+{
 
     GUIStyle doneStyle;
     GUIStyle normalStyle;
@@ -13,16 +25,21 @@ public class ToDoListEditor : Editor {
     [MenuItem("Rezky Tools/To Do List &d")]
     static void ShowToDoList()
     {
+        Selection.activeObject = ShowToDoList(true);
+    }
+
+    public static ToDoList ShowToDoList(bool createIfNotFound = false)
+    {
         const string filePath = "Assets/ToDoList.asset";
         ToDoList toDoList = AssetDatabase.LoadAssetAtPath<ToDoList>(filePath);
-        if (toDoList == null)
+        if (toDoList == null && createIfNotFound)
         {
             toDoList = CreateInstance<ToDoList>();
             AssetDatabase.CreateAsset(toDoList, filePath);
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
         }
-        Selection.activeObject = toDoList;
+        return toDoList;
     }
 
     public override void OnInspectorGUI()
@@ -31,7 +48,7 @@ public class ToDoListEditor : Editor {
 
         InitializeGUIStyle();
 
-        if (todo.list != null && todo.list.Count > 0)
+        if (todo.list.Count > 0)
         {
             ToDoList.Item item;
             for (int i = 0; i < todo.list.Count; i++)
@@ -39,7 +56,7 @@ public class ToDoListEditor : Editor {
                 item = todo.list[i];
                 EditorGUILayout.BeginHorizontal();
                 item.isDone = GUILayout.Toggle(item.isDone, "", GUILayout.ExpandWidth(false));
-                item.task = EditorGUILayout.TextArea(item.task, item.isDone? doneStyle : normalStyle);
+                item.task = EditorGUILayout.TextArea(item.task, item.isDone ? doneStyle : normalStyle);
                 if (GUILayout.Button("x", GUILayout.Width(20)))
                 {
                     Undo.RecordObject(todo, "Remove Task");
